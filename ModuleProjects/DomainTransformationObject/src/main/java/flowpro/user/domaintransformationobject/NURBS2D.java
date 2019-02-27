3dd5e1280cae649663b79cc92ff7da02561eeea2
ondrabublik <obublik@ntis.zcu.cz>
2019-02-27 16:04:10 +0100

    new damping option

--- ModuleProjects/DomainTransformationObject/src/main/java/flowpro/user/domaintransformationobject/NURBS2D.java
+++ ModuleProjects/DomainTransformationObject/src/main/java/flowpro/user/domaintransformationobject/NURBS2D.java
@@ -21,9 +21,14 @@ public class NURBS2D implements DomainTransformationObject {
     public double[] transform(double[] X){
         double tx = X[0];
         double ty = X[1];
-        double x = 3*tx;
-        double bump = 0.15 * Math.exp(-(tx - 0.5) * (tx - 0.5) / 0.02);
-        double y = (1-bump)*ty + bump*(1-ty);
+        
+        double x = tx;
+        double bump = 0.1 * Math.exp(-(tx - 1.5) * (tx - 1.5) / 0.02);
+        double y = ty + bump*(1-ty);
+        
+//        double x = 3*tx;
+//        double bump = 0.1 * Math.exp(-(tx - 0.5) * (tx - 0.5) / 0.02);
+//        double y = (1-bump)*ty + bump*(1-ty);
         return new double[]{x,y};
         //return X;
     }
